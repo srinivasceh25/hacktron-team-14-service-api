8ee0dc0d1a00ae109c8161a081f6fa5d7af3f9de
Gavva, Sivaprasad (Siva) <prampu531@gmail.com>
2019-08-10 15:03:37 +0530

    added CORS allow headerr
    added CORS allow headerr

--- src/main/java/com/hacktron/sqlsetup/controller/QueueDetailController.java
+++ src/main/java/com/hacktron/sqlsetup/controller/QueueDetailController.java
@@ -12,7 +12,7 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/detail")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class QueueDetailController {
 
     @Autowired
